99905fcbd0b6e064f0cceb63b9dcd2a93e615937
Wayne Cassity <webdev.cassity@gmail.com>
2019-12-05 12:19:19 -0800

    getFullNames Passed; -- forEach to loop though & push to order last-name, first-name

--- index.js
+++ index.js
@@ -183,8 +183,13 @@ function processDuplicateFree(list, callback) {
  * @returns an array with all the runners' full names in the following format: "Smith, John".
  * The full names appear in the array in the same order the runners appear in the `runners` array.
 */
-function getFullNames(/* CODE HERE */) {
-  /* CODE HERE */
+function getFullNames(runners) {
+
+  let names = [];
+  runners.forEach((elem) => {
+    names.push(`${elem.last_name}, ${elem.first_name}`);
+  });
+  return names;
 }
 
 /**
